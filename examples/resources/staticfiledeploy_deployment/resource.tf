5e1ae2a5fd26c87a67beb83129e63cf9a2947d9c
Nicolas Harlem Eide <nicolas@harlemeide.net>
2023-11-27 14:43:15 +0100

    Update depoyment resource example

--- examples/resources/staticfiledeploy_deployment/resource.tf
+++ examples/resources/staticfiledeploy_deployment/resource.tf
@@ -2,8 +2,12 @@ data "vy_artifact_version" "this" {
   application = "petstore"
 }
 
-resource "static_file_deploy_deployment" "example_deployment" {
+data "aws_s3_bucket" "website_bucket" {
+  bucket = "123456789012-my-cool-bucket"
+}
+
+resource "staticfiledeploy_deployment" "example_deployment" {
   source         = "${data.vy_artifact_version.this.store}/${data.vy_artifact_version.this.path}"
   source_version = data.vy_artifact_version.this.version
-  target         = "1234567890-website"
+  target         = data.aws_s3_bucket.website_bucket.bucket
 }
